5667446eb583fbe3a92699d754c6bf1b3ad2d6c0
numacci <it.mkgoat.yagi@gmail.com>
2021-01-08 06:44:44 +0900

    template files formatted.

--- src/main/java/com/numacci/api/RestapiPostgresCrudTutorialApplication.java
+++ src/main/java/com/numacci/api/RestapiPostgresCrudTutorialApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestapiPostgresCrudTutorialApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestapiPostgresCrudTutorialApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(RestapiPostgresCrudTutorialApplication.class, args);
+  }
 }
